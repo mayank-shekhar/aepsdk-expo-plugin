afa94b7148f4dc6809a66d80d4d3ae09f54255c7
Mayank Shekhar <6227966+mayank-shekhar@users.noreply.github.com>
2024-08-29 14:20:18 +0530

    Partial working code

--- plugin/src/withCoreiOS.ts
+++ plugin/src/withCoreiOS.ts
@@ -92,6 +92,7 @@ const withCoreXcodeProject: ConfigPlugin<SdkConfigurationProps> = (
           buildSettings[ 'OTHER_CPLUSPLUSFLAGS' ].push("-fcxx-modules");
           buildSettings['OTHER_CPLUSPLUSFLAGS'].push("-Wno-module-import-in-extern-c");
         }
+        buildSettings['LD_RUNPATH_SEARCH_PATHS'] = ["/usr/lib/swift", INHERITED];
       }
     }
     config.modResults = pbxproj;
@@ -188,47 +189,6 @@ ${importCode}
 }
 @end`);
 
-    // Update the xcode project file to include the AdobeBridge files
-    // const pbxproj = modResults;
-    // const fileRef = pbxproj.generateUuid();
-    // console.log("fileRef: ", fileRef);
-    // const bridgeHeaderFile = {
-    //   fileRef,
-    //   group: 'Sources',
-    //   isBuildFile: true,
-    //   basename: 'AdobeBridge.h',
-    //   settings: {ATTRIBUTES: ['Public']}
-    // };
-    // const bridgeImplementationFile = {
-    //   fileRef,
-    //   group: 'Sources',
-    //   isBuildFile: true,
-    //   basename: 'AdobeBridge.m',
-    //   settings: {ATTRIBUTES: ['Public']}
-    // };
-    // pbxproj.addToPbxFileReferenceSection(bridgeHeaderFile);
-    // pbxproj.addToPbxFileReferenceSection(bridgeImplementationFile);
-    // pbxproj.addToPbxBuildFileSection(bridgeHeaderFile);
-    // pbxproj.addToPbxBuildFileSection(bridgeImplementationFile);
-    // pbxproj.addToPbxSourcesBuildPhase(bridgeHeaderFile);
-    // pbxproj.addToPbxSourcesBuildPhase(bridgeImplementationFile);
-    // pbxproj.addToPbxGroup('Sources', bridgeHeaderFile);
-    // pbxproj.addToPbxGroup('Sources', bridgeImplementationFile);
-    // pbxproj.addToLibrarySearchPaths('$(inherited)');
-    // pbxproj.addToHeaderSearchPaths('$(inherited)');
-    // pbxproj.addToFrameworkSearchPaths('$(inherited)');
-    // pbxproj.addToOtherCFlags('$(inherited)');
-    // pbxproj.addToOtherSwiftFlags('$(inherited)');
-    // pbxproj.addToBuildSettings('HEADER_SEARCH_PATHS', '"$(inherited)"');
-    // pbxproj.addToBuildSettings('FRAMEWORK_SEARCH_PATHS', '"$(inherited)"');
-    // pbxproj.addToBuildSettings('OTHER_CFLAGS', '"$(inherited)"');
-    // pbxproj.addToBuildSettings('OTHER_SWIFT_FLAGS', '"$(inherited)"');
-    // pbxproj.addToBuildSettings('SWIFT_VERSION', '5.0');
-    // pbxproj.addToBuildSettings('CLANG_ENABLE_MODULES', 'YES');
-    // pbxproj.addToBuildSettings('CLANG_ENABLE_MODULE_IMPLEMENTATION', 'YES');
-    // pbxproj.addToBuildSettings('SWIFT_OBJC_BRIDGING_HEADER', 'ExpoBridge-Bridging-Header.h');
-
-
     // Update AppDelegate.h to include #import <ExpoModulesCore/EXAppDelegateWrapper.h> if it doesn't already
     const appDelegateHeaderPath = path.join(projectPath, 'AppDelegate.h');
     let appDelegateHeader = fs.readFileSync(appDelegateHeaderPath, 'utf8');
@@ -284,152 +244,152 @@ export const withUpdatedAppDelegate: ConfigPlugin<SdkConfigurationProps> = (
   });
 };
 
-export const withCoreAppDelegate: ConfigPlugin<SdkConfigurationProps> = (
-  config,
-  props,
-) => {
-  return withAppDelegate(config, (config) => {
-    const { logLevel} = props;
-    let appDelegate = config.modResults.contents;
+// export const withCoreAppDelegate: ConfigPlugin<SdkConfigurationProps> = (
+//   config,
+//   props,
+// ) => {
+//   return withAppDelegate(config, (config) => {
+//     const { logLevel} = props;
+//     let appDelegate = config.modResults.contents;
 
 
-    // set the log level based on props.logLevel
-    let logLevelCode = `  `;
-    if (logLevel == "DEBUG") {
-      logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelDebug];`
-    } else if (logLevel == "ERROR") {
-      logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelError];`
-    } else if (logLevel == "WARNING") {
-      logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelWarning];`
-    } else if (logLevel == "VERBOSE" || logLevel == "INFO") {
-      logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelTrace];`
-    } else {
-      logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelError];`
-    }
+//     // set the log level based on props.logLevel
+//     let logLevelCode = `  `;
+//     if (logLevel == "DEBUG") {
+//       logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelDebug];`
+//     } else if (logLevel == "ERROR") {
+//       logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelError];`
+//     } else if (logLevel == "WARNING") {
+//       logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelWarning];`
+//     } else if (logLevel == "VERBOSE" || logLevel == "INFO") {
+//       logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelTrace];`
+//     } else {
+//       logLevelCode = `[AEPMobileCore setLogLevel:AEPLogLevelError];`
+//     }
 
-    // check if the log level code is already added then remove it and add updated code
-    if (appDelegate.includes("[AEPMobileCore setLogLevel:")) {
-      appDelegate = appDelegate.replace(
-        /\[AEPMobileCore setLogLevel:[\s\S]*?\];/,
-        logLevelCode
-      );
-    } else {
-      // Add the log level code to the didFinishLaunchingWithOptions function
-      appDelegate = appDelegate.replace(
-        /self.initialProps = @{};/,
-        `self.initialProps = @{};\n${logLevelCode}\n`
-      );
-    }
+//     // check if the log level code is already added then remove it and add updated code
+//     if (appDelegate.includes("[AEPMobileCore setLogLevel:")) {
+//       appDelegate = appDelegate.replace(
+//         /\[AEPMobileCore setLogLevel:[\s\S]*?\];/,
+//         logLevelCode
+//       );
+//     } else {
+//       // Add the log level code to the didFinishLaunchingWithOptions function
+//       appDelegate = appDelegate.replace(
+//         /self.initialProps = @{};/,
+//         `self.initialProps = @{};\n${logLevelCode}\n`
+//       );
+//     }
 
-    // Read the application dependencies from package.json and add the imports in AppDelegate.m
-    const packageJsonPath = path.resolve(process.cwd(), "package.json");
-    const packageJson = JSON.parse(fs
-      .readFileSync(packageJsonPath, "utf8")
-    );
+//     // Read the application dependencies from package.json and add the imports in AppDelegate.m
+//     const packageJsonPath = path.resolve(process.cwd(), "package.json");
+//     const packageJson = JSON.parse(fs
+//       .readFileSync(packageJsonPath, "utf8")
+//     );
 
-    const dependencies = packageJson.dependencies;
+//     const dependencies = packageJson.dependencies;
 
-    let extensions = `AEPMobileIdentity.class, AEPMobileLifecycle.class, AEPMobileSignal.class`;
+//     let extensions = `AEPMobileIdentity.class, AEPMobileLifecycle.class, AEPMobileSignal.class`;
 
-    const importsToAdd = ['@import AEPCore;', '@import AEPLifecycle;', '@import AEPSignal;', '@import AEPIdentity;'];
-    for (const [name] of Object.entries(dependencies)) {
-      if (name.startsWith("@adobe/react-native-aepcore")) {
-        continue;
-      } else {
-        // check if name is in the iosSdkMap
-        if (iosSdkMap[ name ]) {
-          importsToAdd.push(`@import ${iosSdkMap[ name ]};`);
-          extensions = extensions + `, ${iosSdkClassMap[ name ]}.class`;
-        }
-      }
-    }
+//     const importsToAdd = ['@import AEPCore;', '@import AEPLifecycle;', '@import AEPSignal;', '@import AEPIdentity;'];
+//     for (const [name] of Object.entries(dependencies)) {
+//       if (name.startsWith("@adobe/react-native-aepcore")) {
+//         continue;
+//       } else {
+//         // check if name is in the iosSdkMap
+//         if (iosSdkMap[ name ]) {
+//           importsToAdd.push(`@import ${iosSdkMap[ name ]};`);
+//           extensions = extensions + `, ${iosSdkClassMap[ name ]}.class`;
+//         }
+//       }
+//     }
 
-    // check if importToAdd is empty, means RN SDKs are not installed in the project, raise an error
-    if (importsToAdd.length === 0) {
-      throw new Error("No SDKs found in package.json. Please add the SDKs to the project.")
-    } else {
-      if (!importsToAdd.includes('@import AEPServices;')) {
-        importsToAdd.push('@import AEPServices;')
-      }
-    }
+//     // check if importToAdd is empty, means RN SDKs are not installed in the project, raise an error
+//     if (importsToAdd.length === 0) {
+//       throw new Error("No SDKs found in package.json. Please add the SDKs to the project.")
+//     } else {
+//       if (!importsToAdd.includes('@import AEPServices;')) {
+//         importsToAdd.push('@import AEPServices;')
+//       }
+//     }
 
-    // Add AEP SDK import code in app delegate file
-    const importCode = importsToAdd.join("\n");
+//     // Add AEP SDK import code in app delegate file
+//     const importCode = importsToAdd.join("\n");
 
-    // check if import code is already added then remove it and add updated code
-    if (appDelegate.includes("@import AEPCore;")) {
-      appDelegate = appDelegate.replace(
-        /@import AEPCore[\s\S]*@import AEPServices;/,
-        importCode
-      );
-    } else {
-      // Add the import statements to the app delegate file at the top
-      appDelegate = appDelegate.replace(
-        /#import "AppDelegate.h"/,
-        `#import "AppDelegate.h"\n// Added by Adobe Expo SDK Plugin\n${importCode}`
-      );
-    }
+//     // check if import code is already added then remove it and add updated code
+//     if (appDelegate.includes("@import AEPCore;")) {
+//       appDelegate = appDelegate.replace(
+//         /@import AEPCore[\s\S]*@import AEPServices;/,
+//         importCode
+//       );
+//     } else {
+//       // Add the import statements to the app delegate file at the top
+//       appDelegate = appDelegate.replace(
+//         /#import "AppDelegate.h"/,
+//         `#import "AppDelegate.h"\n// Added by Adobe Expo SDK Plugin\n${importCode}`
+//       );
+//     }
 
-    // Add SDK initialization code in the didFinishLaunchingWithOptions function in Objective-C
-    const sdkInit = `
+//     // Add SDK initialization code in the didFinishLaunchingWithOptions function in Objective-C
+//     const sdkInit = `
 
-  [AEPMobileCore configureWithAppId:@"${props.environmentFileId}"];
-  const UIApplicationState appState = application.applicationState;
-  [AEPMobileCore registerExtensions:@[${extensions}] completion:^{
-    // Use the extensions in your app
-    NSLog(@"Extensions registered successfully");
-    [AEPMobileCore lifecycleStart:@{@"contextDataKey": @"contextDataVal"}];
-    if (appState != UIApplicationStateBackground) {
-      [AEPMobileCore lifecycleStart:nil];
-    }
-  }];
-  `
-
-
-
-
-  //   // Add the SDK initialization code in the didFinishLaunchingWithOptions function
-  //   const sdkInit = `
-  // MobileCore.registerExtensions([${extensions}]) {
-  //   // Added by Adobe Expo Config Module
-  //   // Use the extensions in your app
-  //   MobileCore.registerWith(appId: "${props.environmentFileId}")
-  //   print("Extensions registered successfully")
-
-  //   if application.applicationState != .background {
-  //     // Only start lifecycle if the application is not in the background
-  //     MobileCore.lifecycleStart(additionalContextData: ["contextDataKey": "contextDataVal"])
-  //   }
-  // }
-  //   `;
+//   [AEPMobileCore configureWithAppId:@"${props.environmentFileId}"];
+//   const UIApplicationState appState = application.applicationState;
+//   [AEPMobileCore registerExtensions:@[${extensions}] completion:^{
+//     // Use the extensions in your app
+//     NSLog(@"Extensions registered successfully");
+//     [AEPMobileCore lifecycleStart:@{@"contextDataKey": @"contextDataVal"}];
+//     if (appState != UIApplicationStateBackground) {
+//       [AEPMobileCore lifecycleStart:nil];
+//     }
+//   }];
+//   `
 
 
 
 
-    if (appDelegate.includes("AEPMobileCore registerExtensions:@[")) {
+//   //   // Add the SDK initialization code in the didFinishLaunchingWithOptions function
+//   //   const sdkInit = `
+//   // MobileCore.registerExtensions([${extensions}]) {
+//   //   // Added by Adobe Expo Config Module
+//   //   // Use the extensions in your app
+//   //   MobileCore.registerWith(appId: "${props.environmentFileId}")
+//   //   print("Extensions registered successfully")
 
-      // replace the extensions with new extensions string
-      appDelegate = appDelegate.replace(
-        /AEPMobileCore registerExtensions:@\[[\s\S]*?\] completion/,
-        `AEPMobileCore registerExtensions:@[${extensions}] completion`
-      );
-
-    } else {
-      // add the code to the didFinishLaunchingWithOptions function
-      appDelegate = appDelegate.replace(
-        /self.initialProps = @{};/,
-        `self.initialProps = @{};\n${sdkInit}`
-      );
-    }
-    config.modResults.contents = appDelegate;
-
-    return config
-
-  });
+//   //   if application.applicationState != .background {
+//   //     // Only start lifecycle if the application is not in the background
+//   //     MobileCore.lifecycleStart(additionalContextData: ["contextDataKey": "contextDataVal"])
+//   //   }
+//   // }
+//   //   `;
 
 
 
-}
+
+//     if (appDelegate.includes("AEPMobileCore registerExtensions:@[")) {
+
+//       // replace the extensions with new extensions string
+//       appDelegate = appDelegate.replace(
+//         /AEPMobileCore registerExtensions:@\[[\s\S]*?\] completion/,
+//         `AEPMobileCore registerExtensions:@[${extensions}] completion`
+//       );
+
+//     } else {
+//       // add the code to the didFinishLaunchingWithOptions function
+//       appDelegate = appDelegate.replace(
+//         /self.initialProps = @{};/,
+//         `self.initialProps = @{};\n${sdkInit}`
+//       );
+//     }
+//     config.modResults.contents = appDelegate;
+
+//     return config
+
+//   });
+
+
+
+// }
 
 export const withCoreiOSSdk: ConfigPlugin<SdkConfigurationProps> = (
   config,
